9f52dcb4a20d87c7de67d5c34655eeb597d1bedd
Nico Stranquist <nstranquist@gmail.com>
2022-05-09 17:00:27 -0500

    refactor single course to a list of courses, still using mock data

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -2,28 +2,44 @@ import { Box, Card, CardContent, Typography, CardActions, Button } from "@materi
 
 const exampleCourse = {
   name: 'Physics',
-  courseNumber: 104,
-  score: 95
+  number: 104,
+  score: 86
 }
+const exampleCourses = [
+  {...exampleCourse},
+  {
+    name: 'Math',
+    number: 201,
+    score: 75
+  },
+  {
+    name: 'English',
+    number: 404,
+    score: 91
+  }
+]
 
 export const Courses = () => {
 
   return (
     <Box style={{marginTop: 20}}>
       <Typography variant="h4" component="h2" gutterBottom>Courses</Typography>
-      
+
       {/* https://v4.mui.com/components/cards/#card */}
-      <Card>
-        <CardContent>
-          {/* <Typography className={classes.title} color="textSecondary" gutterBottom>{exampleCourse.name}</Typography> */}
-          <Typography gutterBottom variant="h5" component="h3">{exampleCourse.name}</Typography>
-          <Typography>{exampleCourse.courseNumber}</Typography>
-          <Typography>{exampleCourse.score}</Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">View More</Button>
-        </CardActions>
-      </Card>
+      <Box flexDirection="column" alignItems="center">
+        {exampleCourses.map((course) => (
+          <Card key={course.name} elevation={2} style={{marginTop: 12, marginBottom: 12}}>
+            <CardContent style={{display: 'flex', flexDirection:'row', alignItems: 'center'}}>
+              {/* <Typography className={classes.title} color="textSecondary" gutterBottom>{exampleCourse.name}</Typography> */}
+              <Typography variant="h5" component="h3" style={{flex: 1}}>{course.name} - {course.number}</Typography>
+              <Typography variant="h6" component="h6">{course.score}%</Typography>
+            </CardContent>
+            <CardActions>
+              <Button color="primary">View More</Button>
+            </CardActions>
+          </Card>
+        ))}
+      </Box>
     </Box>
   )
 }
